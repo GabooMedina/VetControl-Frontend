3dad9be9cf8c5957ad7193036c9e781309dbe97e
D4rkgh057 <danielfuelpaz@outlook.com>
2025-05-26 22:05:11 -0500

    Se remueve el boton extra para el registro

--- src/auth/SignIn.tsx
+++ src/auth/SignIn.tsx
@@ -101,11 +101,7 @@ const SignIn = () => {
                   {showPassword ? <FaEyeSlash size={18} /> : <FaEye size={18} />}
                 </button>
               </div>
-              <div className="text-right mb-6">
-                <button type="button" className="text-sm text-gray-600 hover:text-blue-900">
-                  Registrate
-                </button>
-              </div>
+              
               <button
                 type="submit"
                 className="w-full p-3 bg-gray-300 hover:bg-gray-400 rounded text-gray-800 font-medium text-base md:text-lg transition-colors"
