cf334cb322b8be46f78e2b6a09da528d44f4e27d
D4rkgh057 <danielfuelpaz@outlook.com>
2025-05-27 15:30:08 -0500

    Refactor servicios: actualizar fetchAppointments para usar API_URL y agregar getMascotas y getUsuarios

--- src/components/modules/schedule/services/appointmentService.ts
+++ src/components/modules/schedule/services/appointmentService.ts
@@ -1,6 +1,20 @@
 import axios from "axios";
+const API_URL: string = import.meta.env.VITE_BASE_URL;
 
 export async function fetchAppointments() {
-  const response = await axios.get("https://vet-control-backend.onrender.com/citas");
+  const response = await axios.get(`${API_URL}/citas`, {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
+  });
+  return response.data;
+}
+
+export async function createAppointment(appointmentData: any) {
+  const response = await axios.post(`${API_URL}/citas`, appointmentData, {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
+  });
   return response.data;
 }
---
+++ src/components/modules/schedule/services/mascotaService.ts
@@ -0,0 +1,12 @@
+import axios from "axios";
+const API_URL: string = import.meta.env.VITE_BASE_URL;
+
+export const getMascotas = async () => {
+  const token = localStorage.getItem("token");
+  const response = await axios.get(`${API_URL}/mascotas`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return response.data;
+};
